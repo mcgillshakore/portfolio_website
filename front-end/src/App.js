20ccf03432e2bace727f8d33b341ff20ff071124
mcgillshakore <mcgills2093@gmail.com>
2020-10-27 17:51:38 -0500

    added component files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Nav"
+import Home from "./components/Home"
+// import About from "./components/About"
+// import Portfolio from "./components/Portfolio"
+// import Contact from "./components/Contact"
+// import Blog from "./components/Blog"
+import { Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Nav/>
+      </div>
     </div>
   );
 }
