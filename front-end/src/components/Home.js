352f9fbc9ec0c0cf04dc5ab0184a69d29dd663b5
mcgillshakore <mcgills2093@gmail.com>
2020-10-28 17:38:10 -0500

    working on css

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import "../CSS/Home.css"
 function Home() {
     return (
-        <div>
+        <div className="homepage">
+            <div id="bg-circle-profile"></div>
             <h1>Hi, I'm Shakore McGill</h1>
             <h3>Front-End Developer</h3>
         </div>
