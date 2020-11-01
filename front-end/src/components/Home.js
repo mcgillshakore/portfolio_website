ac806f877f3fb1d11e6b5b8d65164b19da8cb0e8
mcgillshakore <mcgills2093@gmail.com>
2020-10-31 19:58:55 -0500

    working on css v.2

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -3,10 +3,8 @@ import "../CSS/Home.css"
 function Home() {
     return (
         <div className="homepage">
-            <div className="text">
                 <h1>Hi, I'm Shakore McGill</h1>
                 <h3>Front-End Developer</h3>
-            </div>
         </div>
     )
 }
