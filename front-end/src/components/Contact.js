b723ba0ffa2817a034d67f5dae5aa5376ca63e17
mcgillshakore <mcgills2093@gmail.com>
2020-11-04 18:19:41 -0600

    updating contact page

--- front-end/src/components/Contact.js
+++ front-end/src/components/Contact.js
@@ -4,6 +4,9 @@ function Contact() {
     return (
         <div>
             <h1>Contact Page</h1>
+            <h2>LinkedIn: <span>https://www.linkedin.com/in/shakore-mcgill/</span></h2>
+            <h2>Github: <span>https://github.com/mcgillshakore</span></h2>
+            <h2>Twitter: <span>https://twitter.com/mcgillshakore</span></h2>
         </div>
     )
 }
